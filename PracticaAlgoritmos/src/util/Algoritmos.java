4370a8fd4eea23edf6774fbe196d01fdba114449
PROTH3X <edualvaro138@gmail.com>
2024-04-16 17:20:44 +0200

    Proyecto actualizado con metodo esPrimo

--- PracticaAlgoritmos/src/util/Algoritmos.java
+++ PracticaAlgoritmos/src/util/Algoritmos.java
@@ -5,8 +5,8 @@ public abstract class Algoritmos {
 	/**
 	 * Recibe un número entero y devuelve el factorial del numero especificado
 	 * 
-	 * @param numero	el numero del que queremos calcular el factorial
-	 * @return			el factorial del numero especificado
+	 * @param numero el numero del que queremos calcular el factorial
+	 * @return el factorial del numero especificado
 	 */
 	public static int factorial(int numero) {
 		int factorial = 1;
@@ -14,6 +14,25 @@ public abstract class Algoritmos {
 			factorial = factorial * i;
 		}
 		return factorial;
+	}
+
+	
+	
+	/**
+	 * Recibe un número entero y devuelve true o false en función de si es primo o no.
+	 * 
+	 * @param numero el numero del que queremos saber si es primo o no.
+	 * @return true o false en función de si es primo o no el numero especificado
+	 */
+	public static boolean esPrimo(int numero) {
+		boolean primo = true;
+		for (int i = 2; i < numero; i++) {
+			if (numero % i == 0) {
+				primo = false;
+				break;
+			}
+		}
+		return primo;
 	}
 
 }
