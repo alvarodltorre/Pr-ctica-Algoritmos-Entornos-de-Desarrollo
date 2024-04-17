78cd7f1f888d16f7d4c7e0140beeb2796569e204
PROTH3X <edualvaro138@gmail.com>
2024-04-17 17:34:12 +0200

    Proyecto actualizado con Javadoc

--- PracticaAlgoritmos/src/util/Algoritmos.java
+++ PracticaAlgoritmos/src/util/Algoritmos.java
@@ -3,7 +3,7 @@ package util;
 public abstract class Algoritmos {
 
 	/**
-	 * Recibe un número entero y devuelve el factorial del numero especificado
+	 * Recibe un numero entero y devuelve el factorial del numero especificado
 	 * 
 	 * @param numero 	el numero del que queremos calcular el factorial
 	 * @return 			el factorial del numero especificado
@@ -19,10 +19,10 @@ public abstract class Algoritmos {
 	
 	
 	/**
-	 * Recibe un número entero y devuelve true o false en función de si es primo o no.
+	 * Recibe un numero entero y devuelve true o false en funcion de si es primo o no.
 	 * 
 	 * @param numero 	el numero del que queremos saber si es primo o no.
-	 * @return 			true o false en función de si es primo o no el numero especificado
+	 * @return 			true o false en funcion de si es primo o no el numero especificado
 	 */
 	public static boolean esPrimo(int numero) {
 		boolean primo = true;
@@ -37,7 +37,7 @@ public abstract class Algoritmos {
 	
 	
 	/**
-	 * Recibe un número entero y y devuelve el número de fibonacci de dicho número.
+	 * Recibe un numero entero y devuelve el numero de fibonacci de dicho numero.
 	 * 
 	 * @param numero 	el numero del que queremos calculas su numero fibonacci.
 	 * @return 			el numero fibonacci de nuestro numero especificado.
